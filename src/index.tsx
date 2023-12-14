c84f343fb21032447b7124ecde96df970f97c013
Mason Sciotti <mason.sciotti@gmail.com>
2023-12-14 16:18:08 -0500

    Clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,6 @@
 import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
 }
 
 export default {
@@ -53,16 +39,6 @@ export default {
 					type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 					data: {
 						content: `${newText}[.](https://uploads.dailydot.com/57f/6c/9055ebfa31c8550e-e1494349501884.jpg?q=65&auto=format&w=2270&ar=2:1&fit=crop)`
-					}
-				}
-			}
-
-			//photo command id: 1164236010452959342
-			else {
-				response = {
-					type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-					data: {
-						content: 'Fail'
 					}
 				}
 			}
